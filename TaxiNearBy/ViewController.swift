dfc0dafde2669a9bec614ae4955074ae2077c272
Muhammad Anum <anumshk27@gmail.com>
2018-03-23 11:55:51 +0500

    Initial Commit

---
+++ TaxiNearBy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaxiNearBy
+//
+//  Created by Muhammad Anum on 23/03/2018.
+//  Copyright © 2018 myTaxi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
